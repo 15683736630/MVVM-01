72459f5b1c4eb2d3553ea7fa765591ec2ceed9e3
apple <apple@appledeMacBook-Pro.local>
2018-04-18 02:51:56 +0800

    小改动

--- MVVM-01/DataController/MVVMDataController.m
+++ MVVM-01/DataController/MVVMDataController.m
@@ -13,39 +13,60 @@
 
 @implementation MVVMDataController
 
+// 模仿多线程的网络请求
 +(void)requetNetWorkCallBlock:(void (^)(NSArray *))callBlock{
     NSMutableArray *entitys = [NSMutableArray arrayWithCapacity:0];
-    MVVMManagerCellEntity *entity1 = [[MVVMManagerCellEntity alloc]init];
-    entity1.Identifier = @"OneCell";
-    NSDictionary  *dic = @{@"name":@(111),@"content":@"大家都拉开始就暗示健康大了空间大撒来得及阿来得及阿斯兰的骄傲手榴弹"};
-    TestModel  *ts = [TestModel HH_ObjectlWithDictionary:dic];
-    ts.cellHeight = 140;
-   // ts.content = @"大家都拉开始就暗示健康大了空间大撒来得及阿来得及阿斯兰的骄傲手榴弹";
-    entity1.dataList = @[ts];
-    [entitys addObject:entity1];
+    dispatch_group_t downloadGroup = dispatch_group_create();
+    dispatch_group_enter(downloadGroup);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        MVVMManagerCellEntity *entity1 = [[MVVMManagerCellEntity alloc]init];
+        entity1.Identifier = @"OneCell";
+        NSDictionary  *dic = @{@"name":@(111),@"content":@"大家都拉开始就暗示健康大了空间大撒来得及阿来得及阿斯兰的骄傲手榴弹"};
+        TestModel  *ts = [TestModel HH_ObjectlWithDictionary:dic];
+        ts.cellHeight = 140;
+        entity1.dataList = @[ts];
+        [entitys addObject:entity1];
+        NSLog(@"[NSThread currentThread] == %@",[NSThread currentThread]);
+        dispatch_group_leave(downloadGroup);
+    });
+    dispatch_group_enter(downloadGroup);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        TestModel  *ts = [TestModel new];
+        ts.cellHeight = 160;
+        [entitys addObject:[MVVMManagerCellEntity newInstantiationEmtityWithIdentifier:@"TwoCell" DataList:@[ts]]];
+        [entitys addObject:[MVVMManagerCellEntity newInstantiationEmtityWithIdentifier:@"ThreeCell" DataList:@[ts]]];
+        NSLog(@"[NSThread currentThread] == %@",[NSThread currentThread]);
+        dispatch_group_leave(downloadGroup);
+    });
     
-    [entitys addObject:[MVVMManagerCellEntity newInstantiationEmtityWithIdentifier:@"TwoCell" DataList:@[ts]]];
-    [entitys addObject:[MVVMManagerCellEntity newInstantiationEmtityWithIdentifier:@"ThreeCell" DataList:@[ts]]];
-    MVVMManagerCellEntity *entity = [[MVVMManagerCellEntity alloc]init];
-    NSMutableArray  *mAry = [NSMutableArray arrayWithCapacity:0];
-    for (int i = 0; i < 10; i ++) {
-        TestModel  *model = [[TestModel alloc]init];
-        model.content = [self randomCreatChinese:arc4random_uniform(200)];
-        [mAry addObject:model];
-    }
-    entity.dataList = mAry.copy;
-    [entitys addObject:entity];
-    [entitys enumerateObjectsUsingBlock:^(MVVMManagerCellEntity *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        obj.headerView = [[HeaderView alloc]init];
-        obj.headerView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width,45);
-        obj.cellHeaderHeight = 45;
-        obj.cellFooterHeight = 10;
-        ((HeaderView*)obj.headerView).titleLabel.text = [NSString stringWithFormat:@"第%ld行Section",idx];
-    }];
-    
-    if (callBlock) {
-        callBlock(entitys.copy);
-    }
+    dispatch_group_enter(downloadGroup);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        MVVMManagerCellEntity *entity = [[MVVMManagerCellEntity alloc]init];
+        NSMutableArray  *mAry = [NSMutableArray arrayWithCapacity:0];
+        for (int i = 0; i < 10; i ++) {
+            TestModel  *model = [[TestModel alloc]init];
+            model.content = [self randomCreatChinese:arc4random_uniform(200)];
+            [mAry addObject:model];
+        }
+        entity.dataList = mAry.copy;
+        [entitys addObject:entity];
+        NSLog(@"[NSThread currentThread] == %@",[NSThread currentThread]);
+        dispatch_group_leave(downloadGroup);
+    });
+
+    dispatch_group_notify(downloadGroup, dispatch_get_main_queue(), ^{
+      [entitys enumerateObjectsUsingBlock:^(MVVMManagerCellEntity *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            obj.headerView = [[HeaderView alloc]init];
+            obj.headerView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width,45);
+            obj.cellHeaderHeight = 45;
+            obj.cellFooterHeight = 10;
+            ((HeaderView*)obj.headerView).titleLabel.text = [NSString stringWithFormat:@"第%ld行Section",idx];
+        }];
+        if (callBlock) {
+            callBlock(entitys.copy);
+        }
+    });
+   
 }
 
 
--- MVVM-01/Model/MVVMBaseSectionModel.m
+++ MVVM-01/Model/MVVMBaseSectionModel.m
@@ -13,23 +13,21 @@
 
 
 +(BOOL)resolveInstanceMethod:(SEL)sel{
-    //动态解析 实例方法
+
     void(^resolveInstanceBlock)(id, SEL) = ^(id objc_self,SEL objc_cmd){
         NSLog(@"如果实例方法未实现则会执行此方法");
     };
-    //为本类添加实例方法
     class_addMethod([self class], sel, imp_implementationWithBlock(resolveInstanceBlock), "v@:");
     printf("%s\n",sel_getName(sel));
     return YES;
     
 }
 +(BOOL)resolveClassMethod:(SEL)sel{
-    //动态解析 类方法
     void(^resoveClassMethod)(id,SEL) = ^(id objc_self,SEL objc_cmd){
         NSLog(@"未实现的类方法，在这里执行");
     };
-    //为本类添加类方法
     class_addMethod(object_getClass([self class]), sel, imp_implementationWithBlock(resoveClassMethod), "v@:");
+    printf("%s\n",sel_getName(sel));
     return YES;
 }
 
